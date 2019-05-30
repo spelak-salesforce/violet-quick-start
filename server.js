61ce125d3e0149f9f732c88d163c1393b79d8232
spelak-salesforce <spelak@salesforce.com>
2019-05-30 12:19:10 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,6 @@
+'use strict';
+var violetSrvr = require('violet/lib/violetSrvr')('/violet');
+violetSrvr.listAppsAt('/');
+var srvrInstance = violetSrvr.createAndListen(process.env.PORT || 8080);
+violetSrvr.loadScript('gameNight.js', 'trailhead');
+console.log('Waiting for requests...');
